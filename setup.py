0b10e412f174afabb4fa36836455f50abb93e199
Jason Kai <tkai@uwo.ca>
2021-08-31 11:10:09 -0400

    add scripts, reqs for qc

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setuptools.setup(
     include_package_data = True, 
     python_requires = "~=3.7",
     install_requires = [ 
+        "matplotlib~=3.4.3",
+        "nilearn~=0.8.0",
         "snakebids~=0.3.10",
     ]
 )
