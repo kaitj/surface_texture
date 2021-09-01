231de7a96d8c755955d319038de5c486baac837b
Jason Kai <tkai@uwo.ca>
2021-09-01 12:01:35 -0400

    Update script to display mosaic with 5 cuts

--- surftexture/workflow/scripts/viz_regqc.py
+++ surftexture/workflow/scripts/viz_regqc.py
@@ -3,7 +3,7 @@ import matplotlib
 
 matplotlib.use("Agg")
 
-overlay = plot_anat(snakemake.input.t1, display_mode="ortho", dim=-1)
+overlay = plot_anat(snakemake.input.t1, display_mode="mosaic", cut_coords=5, dim=-1)
 overlay.add_contours(snakemake.input.ref, colors="r")
 overlay.savefig(snakemake.output.report)
 overlay.close()
