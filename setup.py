9a2e22958c1ed50c400311f221b834ef93cf2c9f
Jason Kai <tkai@uwo.ca>
2021-09-10 09:21:22 -0400

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = "SurfTexture",
-    version = "0.0.1", 
+    version = "0.1.0", 
     author = "Jason Kai", 
     author_email = "tkai@uwo.ca",
     packages = setuptools.find_packages(),
